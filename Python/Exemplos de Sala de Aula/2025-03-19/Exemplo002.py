60f26ea52145ffc4b902bfe022d4ceb3ad49f1b5
profoswaldo <83677370+profoswaldo@users.noreply.github.com>
2025-03-19 10:27:45 -0300

    Aula

---
+++ Python/Exemplos de Sala de Aula/2025-03-19/Exemplo002.py
@@ -0,0 +1,7 @@
+# DADOS -> PROCESSMANTO - > INFORMAÇÃO
+
+
+valor = 30
+print("valor")
+print('valor')
+print(valor)
---
+++ Python/Exemplos de Sala de Aula/2025-03-19/Exemplo028.py
@@ -0,0 +1,12 @@
+# Faça um algoritmo que leia uma variável e some 5 caso seja 
+# par ou some 8 caso seja ímpar, imprimir o resultado desta operação.
+
+
+valor = int(input("Digite um valor: "))
+
+if valor%2 != 0:
+  resultado = valor + 8
+else:
+  resultado = valor + 5
+
+print("Resultado = " + str(resultado))
